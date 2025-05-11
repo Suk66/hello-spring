82070e8e13ba21cf170147dc3c18910679ba20b6
Suk66 <goawwl66@gmail.com>
2025-05-11 18:07:07 +0900

    스프링 꿀팁

---
+++ src/test/java/hello/hello_spring/service/MemberServiceTest.java
@@ -0,0 +1,28 @@
+package hello.hello_spring.service;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MemberServiceTest {
+
+    MemberService memberService = new MemberService();
+
+    @Test
+    void 회원가입() {
+        // given 머리
+
+        // when 가슴
+
+        // then 배
+
+    }
+
+    @Test
+    void findMembers() {
+    }
+
+    @Test
+    void findOne() {
+    }
+}
