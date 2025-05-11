9d37fa607db106db5f0447bf2ea860bcf3cc35c2
Suk66 <goawwl66@gmail.com>
2025-05-11 18:49:40 +0900

    틀린문제찾기

--- src/test/java/hello/hello_spring/service/MemberServiceTest.java
+++ src/test/java/hello/hello_spring/service/MemberServiceTest.java
@@ -25,8 +25,34 @@ class MemberServiceTest {
         // then 배
         Member findMember = memberService.findOne(saveId).get();
         assertThat(member.getName()).isEqualTo(findMember.getName());
+    }
+
+    @Test
+    public void 중복_회원_예외() {
+        // given
+        Member member1 = new Member();
+        member1.setName("spring");
+        
+        Member member2 = new Member();
+        member2.setName("spring");
+
+        // when
+        memberService.join(member1);
+        try {
+            memberService.join(member2);
+            fail();
+        } catch (IllegalStateException e) {
+            assertThat(e.getMessage()).isEqualTo("이미 존재하는 회원입니다.");
+        }
+
+
+
+
+        // then
+
 
     }
+
 
     @Test
     void findMembers() {
