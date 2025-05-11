5c308251f0202ab1301d3c08fcf9b4951e71b1b2
Suk66 <goawwl66@gmail.com>
2025-05-11 18:53:13 +0900

    IllegalStateException

--- src/test/java/hello/hello_spring/service/MemberServiceTest.java
+++ src/test/java/hello/hello_spring/service/MemberServiceTest.java
@@ -38,12 +38,14 @@ class MemberServiceTest {
 
         // when
         memberService.join(member1);
-        try {
-            memberService.join(member2);
-            fail();
-        } catch (IllegalStateException e) {
-            assertThat(e.getMessage()).isEqualTo("이미 존재하는 회원입니다.");
-        }
+        assertThrows(IllegalStateException.class, () -> memberService.join(member2));
+
+//        try {
+//            memberService.join(member2);
+//            fail();
+//        } catch (IllegalStateException e) {
+//            assertThat(e.getMessage()).isEqualTo("이미 존재하는 회원입니다. ");
+//        }
 
 
 
