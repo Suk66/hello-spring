511190d0885375c747836549add798d666881dde
Suk66 <goawwl66@gmail.com>
2025-05-11 18:36:29 +0900

    TDD

--- src/test/java/hello/hello_spring/service/MemberServiceTest.java
+++ src/test/java/hello/hello_spring/service/MemberServiceTest.java
@@ -1,7 +1,12 @@
 package hello.hello_spring.service;
 
+import hello.hello_spring.domain.Member;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
 class MemberServiceTest {
@@ -11,10 +16,15 @@ class MemberServiceTest {
     @Test
     void 회원가입() {
         // given 머리
+        Member member = new Member();
+        member.setName("hello");
 
         // when 가슴
+        Long saveId =  memberService.join(member);
 
         // then 배
+        Member findMember = memberService.findOne(saveId).get();
+        assertThat(member.getName()).isEqualTo(findMember.getName());
 
     }
 
